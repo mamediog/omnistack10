fd19d9a5230a4e48f778ded2d9ecf63f3914161d
Guilherme Mamedio <mamedioguilherme@gmail.com>
2020-02-13 15:51:06 -0300

    app mobile react

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require ('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://oministack:oministack@cluster0-avj1e.mongodb.net
   useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
